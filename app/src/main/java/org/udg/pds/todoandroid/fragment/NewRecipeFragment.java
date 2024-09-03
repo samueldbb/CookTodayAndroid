28a234457b1f4fc09554cefa187997a329584246
samueldbb <samuelpan44@gmail.com>
2024-09-03 02:28:53 +0200

    canviat estil de nova recepta

--- app/src/main/java/org/udg/pds/todoandroid/fragment/NewRecipeFragment.java
+++ app/src/main/java/org/udg/pds/todoandroid/fragment/NewRecipeFragment.java
@@ -65,44 +65,85 @@ public class NewRecipeFragment extends Fragment implements Callback<IdObject> {
 
         mTodoService = ((TodoApp) this.getActivity().getApplication()).getAPI();
 
-        FragmentManager fm = getParentFragmentManager();
-
         // Register the launcher and define the callback for the result
         imagePickerLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == getActivity().RESULT_OK && result.getData() != null) {
                     selectedImage = result.getData().getData();
-                    binding.ivPreview.setImageURI(selectedImage);
+                    uploadImage();
                 }
             });
 
-        binding.pujarButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    String nom = binding.nomRecepta.getText().toString();
-                    String descripcio = binding.descripcioRecepta.getText().toString();
-                    String passos = binding.passosRecepta.getText().toString();
-                    String ingredients = binding.ingredientsRecepta.getText().toString();
-                    Collection<String> categories = new ArrayList<>();
-                    String imageUrl = imgUrl;
-                    categories.add(binding.categoriaRecepta.getSelectedItem().toString());
-                    ReceptaString recepta = new ReceptaString();
-                    recepta.nom = nom;
-                    recepta.descripcio = descripcio;
-                    recepta.passos = passos;
-                    recepta.ingredients = ingredients;
-                    recepta.categories = categories;
-                    recepta.imageUrl = imageUrl;
-                    Call<IdObject> call = mTodoService.addRecepta(recepta);
-                    call.enqueue(NewRecipeFragment.this);
-                    buida();
-                } catch (Exception ex) {
-                    Toast.makeText(NewRecipeFragment.this.getContext(), "Error pujant la recepta", Toast.LENGTH_LONG).show();
-                }
+        binding.pujarButton.setOnClickListener(v -> {
+            try {
+                String nom = binding.nomRecepta.getText().toString();
+                String descripcio = binding.descripcioRecepta.getText().toString();
+                String passos = binding.passosRecepta.getText().toString();
+                String ingredients = binding.ingredientsRecepta.getText().toString();
+                Collection<String> categories = new ArrayList<>();
+                String imageUrl = imgUrl;
+                categories.add(binding.categoriaRecepta.getSelectedItem().toString());
+                ReceptaString recepta = new ReceptaString();
+                recepta.nom = nom;
+                recepta.descripcio = descripcio;
+                recepta.passos = passos;
+                recepta.ingredients = ingredients;
+                recepta.categories = categories;
+                recepta.imageUrl = imageUrl;
+                Call<IdObject> call = mTodoService.addRecepta(recepta);
+                call.enqueue(NewRecipeFragment.this);
+                buida();
+            } catch (Exception ex) {
+                Toast.makeText(NewRecipeFragment.this.getContext(), "Error pujant la recepta", Toast.LENGTH_LONG).show();
             }
         });
+
         return root;
+    }
+
+    private void uploadImage() {
+        if (selectedImage != null) {
+            try {
+                InputStream is = getContext().getContentResolver().openInputStream(selectedImage);
+                String extension = "." + MimeTypeMap.getSingleton().getExtensionFromMimeType(getContext().getContentResolver().getType(selectedImage));
+                File tempFile = File.createTempFile("upload", extension, getContext().getCacheDir());
+                FileOutputStream outs = new FileOutputStream(tempFile);
+                IOUtils.copy(is, outs);
+                // create RequestBody instance from file
+                RequestBody requestFile =
+                    RequestBody.create(
+                        MediaType.parse(getContext().getContentResolver().getType(selectedImage)),
+                        tempFile
+                    );
+
+                // MultipartBody.Part is used to send also the actual file name
+                MultipartBody.Part body =
+                    MultipartBody.Part.createFormData("file", tempFile.getName(), requestFile);
+
+                Call<String> call = mTodoService.uploadImage(body);
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if (response.isSuccessful()) {
+                            imgUrl = response.body().toString();
+                            Toast.makeText(getContext(), "Imatge pujada correctament!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), "Error al pujar", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(getContext(), "La pujada ha fallat", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (Exception e) {
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "No has seleccionat cap imatge", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void buida() {
@@ -110,78 +151,21 @@ public class NewRecipeFragment extends Fragment implements Callback<IdObject> {
         binding.descripcioRecepta.setText("");
         binding.ingredientsRecepta.setText("");
         binding.passosRecepta.setText("");
-        binding.ivPreview.setImageURI(null);
-        binding.ivDownload.setImageDrawable(null);
         binding.categoriaRecepta.setSelection(0);
 
-        selectedImage= null;
-        imgUrl= "";
+        selectedImage = null;
+        imgUrl = "";
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        mTodoService = ((TodoApp) this.getActivity().getApplication()).getAPI();
-        binding.bSelectImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                imagePickerLauncher.launch(Intent.createChooser(intent, "Selecciona imatge"));
-            }
-        });
-
-        binding.bUploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (selectedImage != null) {
-                    try {
-                        InputStream is = getContext().getContentResolver().openInputStream(selectedImage);
-                        String extension = "." + MimeTypeMap.getSingleton().getExtensionFromMimeType(getContext().getContentResolver().getType(selectedImage));
-                        File tempFile = File.createTempFile("upload", extension, getContext().getCacheDir());
-                        FileOutputStream outs = new FileOutputStream(tempFile);
-                        IOUtils.copy(is, outs);
-                        // create RequestBody instance from file
-                        RequestBody requestFile =
-                            RequestBody.create(
-                                MediaType.parse(getContext().getContentResolver().getType(selectedImage)),
-                                tempFile
-                            );
-
-                        // MultipartBody.Part is used to send also the actual file name
-                        MultipartBody.Part body =
-                            MultipartBody.Part.createFormData("file", tempFile.getName(), requestFile);
-
-                        Call<String> call = mTodoService.uploadImage(body);
-                        call.enqueue(new Callback<String>() {
-                            @Override
-                            public void onResponse(Call<String> call, Response<String> response) {
-                                if (response.isSuccessful()) {
-                                    imgUrl = response.body().toString();
-                                    Toast.makeText(getContext(), "Imatge pujada correctament!", Toast.LENGTH_SHORT).show();
-                                    Picasso.get().load(response.body()).into(binding.ivDownload);
-                                }
-                                else
-                                    Toast.makeText(getContext(), "Error al pujar", Toast.LENGTH_SHORT).show();
-                            }
-
-                            @Override
-                            public void onFailure(Call<String> call, Throwable t) {
-                                Toast.makeText(getContext(), "La pujada ha fallat", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                    catch (Exception e) {
-                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-                else{
-                    Toast.makeText(getContext(), "no has seleccionat res", Toast.LENGTH_SHORT).show();
-                }
-            }
+        binding.bSelectImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            imagePickerLauncher.launch(Intent.createChooser(intent, "Selecciona imatge"));
         });
 
     }
@@ -190,7 +174,6 @@ public class NewRecipeFragment extends Fragment implements Callback<IdObject> {
     public void onResponse(Call<IdObject> call, Response<IdObject> response) {
         if (response.isSuccessful()) {
             Toast.makeText(this.getContext(), "Pujat correctament", Toast.LENGTH_LONG).show();
-
         } else {
             Toast.makeText(this.getContext(), "No s'ha pogut pujar", Toast.LENGTH_LONG).show();
         }
@@ -200,5 +183,4 @@ public class NewRecipeFragment extends Fragment implements Callback<IdObject> {
     public void onFailure(Call<IdObject> call, Throwable t) {
         Toast.makeText(this.getContext(), "Error al pujar la recepta", Toast.LENGTH_LONG).show();
     }
-
 }
